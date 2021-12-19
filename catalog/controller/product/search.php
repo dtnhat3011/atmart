dff8f12cec51423d3c793c8bf42a86124d3ccf71
dtnhat3011 <dothanhnhat91@gmail.com>
2021-12-19 21:56:27 +0700

    fix hide price for search page

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -197,6 +197,10 @@ class ControllerProductSearch extends Controller {
 					$image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
 				}
 
+				if ($this->customer->isLogged()) {
+					$data['logged'] = true;
+				}
+
 				if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
 					$price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
 				} else {
